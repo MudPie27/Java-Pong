72ea4d14ba3d80dcc9e5e4c7ac6ac66fa44a2bf2
MudPie27 <aseerbaset@gmail.com>
2023-05-22 18:07:01 -0400

    Bounce

--- src/Ball.java
+++ src/Ball.java
@@ -1,23 +1,71 @@
 package src;
+
 import java.awt.*;
 
 public class Ball extends Rectangle {
+    public static final int BALL_DIAMETER = 20;
+    private static final int SPEED = 7;
 
-	public final int SPEED = 7;
-	public static final int BALL_DIAMETER = 20;
+    private int velocityX;
+    private int velocityY;
+    private double rand, timer;
 
-	public Ball(int x, int y) {
-		super(x, y, BALL_DIAMETER, BALL_DIAMETER);
-	}
+    public Ball(int x, int y) {
+        super(x, y, BALL_DIAMETER, BALL_DIAMETER);
+        resetVelocity();
+        timer = 0;
+    }
 
-	public void move() {
-		y = y + SPEED;
-		x = x + (int)(Math.random()*14);
-	}
+    private void resetVelocity() {
+		rand = Math.random();
 
-	public void draw(Graphics g) {
-		g.setColor(Color.white);
-		g.fillOval(x, y, BALL_DIAMETER, BALL_DIAMETER);
-	}
+        if (rand < 0.5) {
+            velocityX = -SPEED;
+        } else {
+            velocityX = SPEED;
+        }
 
+        if (rand < 0.5) {
+            velocityY = -SPEED;
+        } else {
+            velocityY = SPEED;
+        }
+    }
+
+    public void move() {
+        x += velocityX;
+        y += velocityY;
+    }
+
+	public void checkPaddleCollision(Paddles paddle1, Paddles paddle2) {
+        if (intersects(paddle1) || intersects(paddle2)) {
+            velocityX = -velocityX; // Reverse the horizontal velocity
+        }
+    }
+
+    public void checkBoundaryCollision() {
+        if (y <= 0 || y >= GamePanel.GAME_HEIGHT - BALL_DIAMETER) {
+            velocityY = -velocityY; // Reverse the vertical velocity
+        }
+
+        // Check if the ball has gone off-screen
+        if (x <= 0 || x >= GamePanel.GAME_WIDTH - BALL_DIAMETER) {
+            if (System.currentTimeMillis() - respawnTime >= 1500) {
+                respawn();
+            }
+        } else {
+            respawnTime = System.currentTimeMillis(); // Update the respawn time
+        }
+    }
+
+    public void respawn() {
+        x = GamePanel.GAME_WIDTH / 2 - BALL_DIAMETER / 2;
+        y = GamePanel.GAME_HEIGHT / 2 - BALL_DIAMETER / 2;
+        resetVelocity();
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillOval(x, y, BALL_DIAMETER, BALL_DIAMETER);
+    }
 }
